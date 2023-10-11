c8f81953f67a6f2520b3de3f87ab6aab73b95030
lina abumahfood <lina.abumahfood@gmail.com>
2023-10-11 18:58:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const words = tutorials.map(tutorial => tutorial.split(" "))
+  const capitlizeWords = words.map(tutorial => tutorial.map(el => el.charAt(0).toUpperCase() + el.slice(1)))
+  const tutorialsCapitlized = capitlizeWords.map(el => el.join(" "))
+  return tutorialsCapitlized
 }
+console.log(titleCased());
+
+
